c82bdd265c867f32f7f7f5de41bb7b8553ab597a
Denis Kim <denis-kim@outlook.de>
2026-01-04 14:25:30 +0100

    Dev_1: Customized main file

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+// main.js
+const arr = [1, 2, 3, 5];
+arr[8] = 9;
+
+
 function greet(name) {
-  return `Hello ${name}, from Dev_2!`;
+  return `Hello ${name}, from Dev_1 & Dev_2!`;
 }
 
 console.log(greet("Team"));
 
+console.log(arr);
